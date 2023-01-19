17f21aea77610bf665abac4740d7e8e87634fb9a
Jimmy Emery <jimmy.e@outlook.com>
2023-01-19 14:47:32 -0600

    working on transaction

---
+++ src/constants.ts
@@ -0,0 +1,15 @@
+export const progress = [
+  { label: 'Ready', value: 'ready' },
+  { label: 'Assessing', value: 'assessing' },
+  { label: 'In Progress', value: 'inProgress' },
+  { label: 'In Review', value: 'inReview' },
+  { label: 'Completed', value: 'completed' },
+];
+
+export const status = [
+  { label: 'Pending', value: 'pending' },
+  { label: 'Submitted', value: 'submitted' },
+  { label: 'Approved', value: 'approved' },
+  { label: 'Declined', value: 'declined' },
+  { label: 'Paid', value: 'paid' },
+];
