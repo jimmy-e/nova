9c23335fd0f6645961e41631e51d46af9d0c7a8a
Jimmy Emery <jimmy.e@outlook.com>
2023-01-19 09:41:40 -0600

    updating search

--- src/pages/transactions/filters/Search.tsx
+++ src/pages/transactions/filters/Search.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
-import { EuiFieldText } from '@elastic/eui';
+import React, { useState } from 'react';
+import { EuiFieldText, EuiFormControlLayout } from '@elastic/eui';
+import styles from './Search.module.css';
 
-const Search: React.FC = () => <EuiFieldText />;
+const Search: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>();
+
+  let formControlProps: Record<string, Record<string, () => void>> = {};
+
+  if (searchValue) {
+    formControlProps.clear = {
+      onClick: () => setSearchValue('')
+    };
+  }
+
+  return (
+    <EuiFormControlLayout {...formControlProps}>
+      <EuiFieldText
+        className={styles.fieldText}
+        onChange={(event) => setSearchValue(event.target.value)}
+        value={searchValue}
+      />
+    </EuiFormControlLayout>
+  );
+};
 
 export default Search;
