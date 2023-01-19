4dde2e447189e0ae547b042f7f72df483acd1944
Jimmy Emery <jimmy.e@outlook.com>
2023-01-19 08:53:29 -0600

    updating page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,20 @@ import { Inter } from '@next/font/google'
 import '@elastic/eui/dist/eui_theme_light.css';
 import { EuiProvider, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import Transactions from './transactions/Transactions';
+import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <EuiProvider colorMode="light">
+      <div className={styles.main}>
         <EuiFlexGroup alignItems="center" justifyContent="center">
           <EuiFlexItem>
             <Transactions />
           </EuiFlexItem>
         </EuiFlexGroup>
+      </div>
     </EuiProvider>
   )
 }
