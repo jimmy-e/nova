4cb291279a8450b2112930e53847a6e540905507
Jimmy Emery <jimmy.e@outlook.com>
2023-01-19 14:00:23 -0600

    working on app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,16 @@
-import { Inter } from '@next/font/google'
+import React from 'react';
 import '@elastic/eui/dist/eui_theme_light.css';
 import { EuiProvider } from '@elastic/eui';
 import Transactions from './transactions/Transactions';
 import styles from '@/styles/Home.module.css'
 import styleOverrides from '@/styles/styleOverrides';
 
-const inter = Inter({ subsets: ['latin'] })
+const App: React.FC =() => (
+  <EuiProvider colorMode="light" modify={styleOverrides}>
+    <div className={styles.main}>
+      <Transactions />
+    </div>
+  </EuiProvider>
+);
 
-export default function Home() {
-  return (
-    <EuiProvider colorMode="light" modify={styleOverrides}>
-      <div className={styles.main}>
-        <Transactions />
-      </div>
-    </EuiProvider>
-  )
-}
+export default App;
