64508eb16207c533e1e8d308925c98e4c5da269f
Jimmy Emery <jimmy.e@outlook.com>
2023-01-19 08:57:37 -0600

    updating header

--- src/pages/transactions/Header.tsx
+++ src/pages/transactions/Header.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import { EuiPage, EuiButton, EuiProvider } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
+import styles from './Header.module.css';
 
 const Header: React.FC = () => (
-  <h1>header</h1>
+  <EuiFlexGroup direction="column" gutterSize="m">
+    <EuiFlexItem grow={false}>
+      <EuiText>
+        <h1>Transactions</h1>
+      </EuiText>
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiText>
+        <h3 className={styles.subtitle}>
+          Your business contracts, including fund subscriptions and applications.
+        </h3>
+      </EuiText>
+    </EuiFlexItem>
+  </EuiFlexGroup>
 );
 
 export default Header;
